03969c68ca8fe4f1c48f51a9ef2792f0893a4bc8
Paula Oliveira <oliveirapaulaa@gmail.com>
2015-03-26 01:18:11 -0300

    navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+$(document).ready(function(){
+	var nav = $('.header-content');
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 56) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+    $('.menu').onePageNav();
+});
