4aa6fb03763a385cd4f8d150721bf982a201eb6b
Paula Oliveira <oliveirapaulaa@gmail.com>
2015-03-27 16:34:01 -0300

    final commit

--- js/main.js
+++ js/main.js
@@ -9,5 +9,28 @@ $(document).ready(function(){
             nav.removeClass("f-nav");
         }
     });
-    $('.menu').onePageNav();
+    $('.menu, .slideout-menu ul').onePageNav();
+    
+     $('.slideout-menu-toggle').on('click', function(event){
+    	event.preventDefault();
+    	// create menu variables
+    	var slideoutMenu = $('.slideout-menu');
+    	var slideoutMenuWidth = $('.slideout-menu').width();
+    	
+    	// toggle open class
+    	slideoutMenu.toggleClass("open");
+    	
+    	// slide menu
+    	if (slideoutMenu.hasClass("open")) {
+	    	slideoutMenu.animate({
+		    	left: "0px"
+	    	});	
+    	} else {
+	    	slideoutMenu.animate({
+		    	left: -slideoutMenuWidth
+	    	}, 250);	
+    	}
+    });
+
+    
 });
